e38a3d554bca74a353ebb805c0c9bcca8351363f
Amandeep Singh <109553527+Amanespana@users.noreply.github.com>
2023-05-27 16:50:07 -0400

    Update pizza_types_queries.sql

--- pizza_types_queries.sql
+++ pizza_types_queries.sql
@@ -1,6 +1,6 @@
--- SELECT * FROM pizza_sales.pizza_types;
--- SELECT count(*) FROM pizza_sales.pizza_types;
--- select count(distinct(pizza_type_id)) FROM pizza_sales.pizza_types;
--- select distinct(category) FROM pizza_sales.pizza_types;
--- select ingredients from pizza_sales.pizza_types;
--- select category, count(category), count(category)*100/32 as contribution from pizza_sales.pizza_types group by category;
+SELECT * FROM pizza_sales.pizza_types;
+SELECT count(*) FROM pizza_sales.pizza_types;
+select count(distinct(pizza_type_id)) FROM pizza_sales.pizza_types;
+select distinct(category) FROM pizza_sales.pizza_types;
+select ingredients from pizza_sales.pizza_types;
+select category, count(category), count(category)*100/32 as contribution from pizza_sales.pizza_types group by category;
