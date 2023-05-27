cd67ec7aa072e523e2a775160bf22b63c9121ef2
Amanespana <amandeep1552@gmail.com>
2023-05-27 16:22:12 -0400

    Initial Commit

---
+++ pizzas_queries.sql
@@ -0,0 +1,11 @@
+-- SELECT * FROM pizza_sales.pizzas;
+-- SELECT count(*) FROM pizza_sales.pizzas;
+-- select distinct(price) FROM pizza_sales.pizzas where size = 'S' order by price asc;
+-- select * from pizza_sales.pizzas where price = 23.65;
+-- select * from pizza_sales.pizzas where pizza_type_id = 'brie_carre';
+-- select distinct(size) from pizza_sales.pizzas;
+-- select max(price) from pizza_sales.pizzas;
+-- select * from pizza_sales.pizzas order by price desc;
+-- select size, count(pizza_id) from pizza_sales.pizzas group by size;
+-- select count(distinct(pizza_type_id)) from pizza_sales.pizzas;
+-- select count(distinct(pizza_type_id)) from pizza_sales.pizzas;
